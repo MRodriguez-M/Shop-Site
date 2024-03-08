eda6d599bca765ff9dbe26eb8871127423faac0d
Melissa Rodriguez <melissa.rodriguez.murguia@gmail.com>
2024-03-08 12:15:22 -0600

    Added script for listing all products

--- resources/js/mainPage.js
+++ resources/js/mainPage.js
@@ -1,13 +1,30 @@
 let productData = [
-    { title: 'Helicopter Ride', description: 'helicopter ride for 2', price: 100 },
-    { title: 'Make-Up', description: 'full kit of makeup', price: 123 },
-    { title: 'Motorbike Ride', description: 'hour-long bike ride', price: 140 },
-    { title: 'Driver\'s License Lessons', description: '5 classes to help earn driver\'s license', price: 350 },
-    { title: 'Air Balloon Ride', description: 'air ballon ride for 4', price: 235 },
-    { title: 'Massage', description: 'full package massage', price: 75 }
+    { title: 'Helicopter Ride', description: 'helicopter ride for 2', price: '$100' },
+    { title: 'Make-Up', description: 'full kit of makeup', price: '$123' },
+    { title: 'Motorbike Ride', description: 'hour-long bike ride', price: '$140' },
+    { title: 'Driver\'s License Lessons', description: '5 classes to help earn driver\'s license', price: '$350' },
+    { title: 'Air Balloon Ride', description: 'air ballon ride for 4', price: '$235' },
+    { title: 'Massage', description: 'full package massage', price: '$75' },
+    { title: 'Cooking Courses', description: 'get 4 courses for the price of 3', price: '$280' },
 ];
 
 for(let i = 0; i < productData.length; i++) {
-    const product = document.getElementsByClassName('mainPage__product-text')
-    product[i].innerHTML = productData[i].title;
+    const productName = document.getElementsByClassName('mainPage__product-text')
+    const productDescription = document.getElementsByClassName('mainPage__product-text-description')
+    const productPrice = document.getElementsByClassName('mainPage__product-price-text')
+
+    productName[i].innerHTML = productData[i].title;
+    productDescription[i].innerHTML = productData[i].description;
+    productPrice[i].innerHTML = productData[i].price;
+}
+
+const searchItem = () => {
+    let input = document.getElementsByClassName('navbar__search-bar-input').value;
+    let filteredSearch = [];
+
+    for(let i = 0; i < productData.length; i++) {
+        if(productData[i].title.toLowerCase().includes(input.toLowerCase())) {
+            filteredSearch.push(productData[i]);
+        }
+    }
 }
